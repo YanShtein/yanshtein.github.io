eb3a4d2fb567aa01ac8b32ff37566e7ced0632cc
Yan Shtein <85824489+YanShtein@users.noreply.github.com>
2022-07-15 13:14:42 +0300

    project changes

--- index.js
+++ index.js
@@ -2,14 +2,3 @@ $( document ).ready(function() {
   $('#loading').fadeOut(() => {$('#content').fadeIn()})
 });
 
-let project = document.getElementsByClassName('project');
-let overlay = document.getElementsByClassName('overlay');
-
-for (let i = 0; i < project.length; i++) {
-  project[i].addEventListener("mouseover", function() {
-    overlay[i].style.display = "flex";
-  });
-  project[i].addEventListener("mouseout", function() {
-    overlay[i].style.display = "none";
-  });
-};
